aa2101aa6e79a68319b6f90635ac8874252994ae
Hugo Calmels <calmels.hugo@yahoo.fr>
2022-04-05 17:44:10 +0200

    cors issue

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:3000'
+    origins 'https://snake-api-plus.herokuapp.com'
 
     resource '*',
       headers: :any,
